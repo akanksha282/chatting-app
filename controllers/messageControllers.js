5d07905b3fbec2b90a729c89aba423b7af6b5913
akanksha282 <akankshajaiswal003@gmail.com>
2023-12-30 19:23:40 +0530

    suucc

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -1,8 +1,75 @@
+// const asyncHandler = require("express-async-handler");
+// const Message = require("../models/messageModule");
+// const User = require("../models/userModel");
+// const Chat = require("../models/chatModel");
+
+
+// const sendMessage=asyncHandler(async(req,res)=>{
+
+//     const {content,chatId}=req.body;
+//     if(!content || !chatId)
+//     {
+//         console.log("Invalid data passed into request");
+//         return res.sendStatus(400);
+//     }
+
+//     var newMesaage={
+//         sender:req.user._id,
+//         content:content,
+//         chat:chatId,
+//     };
+
+//     try {
+//         var message=await Message.create(newMesaage);
+         
+//         message =await message.populate("sender","name pic");
+//         message=await message.populate("chat");
+          
+
+//         // change 
+//         //  message =await User.populate(message,{
+//         //     path:"chat.users",
+//         //     select:"name pic email",
+//         //  });
+
+
+
+//    await Chat.findByIdAndUpdate(req.body.chatId,{
+//     latestMessage:message,
+//    });
+//    res.json(message);
+        
+//     } catch (error) {
+//         res.status(400);
+//         throw new Error(error.message);
+        
+//     }
+
+
+// });
+
+
+// const allMessages=asyncHandler(async(req,res)=>{
+//      try {
+//     const messages = await Message.find({ chat: req.params.chatId })
+//       .populate("sender", "name pic email")
+//       .populate("chat");
+//     res.json(messages);
+//   } catch (error) {
+//     res.status(400);
+//     throw new Error(error.message);
+//   }
+
+// })
+
+// module.exports={sendMessage,allMessages}; 
 const asyncHandler = require("express-async-handler");
 const Message = require("../models/messageModule");
 const User = require("../models/userModel");
 const Chat = require("../models/chatModel");
-
+const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const app=express();
 
 const sendMessage=asyncHandler(async(req,res)=>{
 
@@ -10,6 +77,7 @@ const sendMessage=asyncHandler(async(req,res)=>{
     if(!content || !chatId)
     {
         console.log("Invalid data passed into request");
+        
         return res.sendStatus(400);
     }
 
@@ -37,9 +105,45 @@ const sendMessage=asyncHandler(async(req,res)=>{
    await Chat.findByIdAndUpdate(req.body.chatId,{
     latestMessage:message,
    });
+    const log={
+    level: "info",
+    message: "Message sent successfully",
+    timestamp: new Date().toISOString(),
+    resourceId: 'sendMessage'+uuidv4(),
+    traceId: uuidv4(), // Replace with actual trace ID logic
+    spanId: uuidv4() , // Replace with actual span ID logic
+    commit: "successh2w",
+  };
+
+  // app.post('http://localhost:3001/ingest',JSON.stringify(log));
+  fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(log),
+        });
    res.json(message);
         
     } catch (error) {
+      const log={
+        level: "error",
+        message: "Error sending message",
+        timestamp: new Date().toISOString(),
+        resourceId:'sendMessage'+uuidv4(), // Replace with actual resource ID logic
+        traceId: uuidv4(), // Replace with actual trace ID logic
+        spanId:  uuidv4(), // Replace with actual span ID logic
+        commit: "Failed", // Replace with actual commit message
+       
+      };
+      // app.post('http://localhost:3001/ingest',JSON.stringify(log));
+      fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(log),
+        });
         res.status(400);
         throw new Error(error.message);
         
@@ -54,8 +158,44 @@ const allMessages=asyncHandler(async(req,res)=>{
     const messages = await Message.find({ chat: req.params.chatId })
       .populate("sender", "name pic email")
       .populate("chat");
+      const log={
+        level: "info",
+        message: "Fetched all messages for chat",
+        timestamp: new Date().toISOString(),
+        resourceId: 'allmessgae'+uuidv4(), // Replace with actual resource ID logic
+        traceId: uuidv4(), // Replace with actual trace ID logic
+        spanId: uuidv4(), // Replace with actual span ID logic
+        commit: "successchatt", // Replace with actual commit message
+        
+      };
+      // app.post('http://localhost:3001/ingest',JSON.stringify(log));
+      fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(log),
+        });
     res.json(messages);
   } catch (error) {
+      const log={
+      level: "error",
+      message: "Error fetching messages",
+      timestamp: new Date().toISOString(),
+      resourceId: undefined,
+      traceId: uuidv4(),
+      spanId: uuidv4(),
+      commit: "error042", // Replace with actual commit message
+    
+    };
+    // app.post('http://localhost:3001/ingest',JSON.stringify(log));
+    fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(log),
+        });
     res.status(400);
     throw new Error(error.message);
   }
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,14 +1,125 @@
+// const asyncHandler=require('express-async-handler');
+// const User= require('../models/userModel');
+// const generateToken= require('../config/generateToken');
+
+
+
+// const registerUser =asyncHandler(async(req,res)=>{
+//     const {name ,email,password ,pic}=req.body;
+
+//     if(!name || !email || !password)
+//     {
+//         res.status(400);
+//         throw new Error('Please Enter all the Fields')
+//     }
+
+//     const userExists =await User.findOne({email:email});
+
+//     if(userExists)
+//     {
+//         res.status(400);
+//         throw new Error('user already exist');
+//     }
+
+//     const user =User({
+//         name,
+//         email,
+//         password,
+//         pic,
+
+//     });
+//     // change
+//      await user.save()
+//     if(user)
+// {
+//     res.status(201).json({
+//         _id:user._id,
+//         name:user.name,
+//         email:user.email,
+//         password:user.password,
+//         pic:user.pic,
+//         token:generateToken(user._id)
+//     });
+
+// }
+// else{
+//     res.status(400);
+//     throw new Error("Failed to create new user")
+// }
+
+// });
+
+// const authUser =asyncHandler(async(req,res)=>{
+//     const {email,password}=req.body;
+
+//     const user =await User.findOne({email:email});
+
+//     if(user && (await user.matchPassword(password)))
+//     {
+//         res.json({
+//              _id:user._id,
+//         name:user.name,
+//         email:user.email,
+//         password:user.password,
+//         pic:user.pic,
+//         token:generateToken(user._id)
+//         })
+//     }
+//     else{
+//     res.status(400);
+//     throw new Error("Invalid id or password")
+// }
+
+// });
+// // /api/user?search=Raj
+// const allUsers= asyncHandler(async (req,res)=>{
+//     const keyword=req.query.search?{
+//         $or:[
+//             {name:{$regex:req.query.search,$options:"i"}},
+//             {email:{$regex:req.query.search,$options:"i"}},
+//         ],
+//     }
+//     :{}
+//     // change from find
+//     const users =await User.find(keyword).find({_id: {$ne: req.user._id}});
+//     res.send(users)
+
+   
+// })
+
+
+
+// module.exports={registerUser ,authUser,allUsers };
 const asyncHandler=require('express-async-handler');
 const User= require('../models/userModel');
 const generateToken= require('../config/generateToken');
-
-
+const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const app=express();
 
 const registerUser =asyncHandler(async(req,res)=>{
     const {name ,email,password ,pic}=req.body;
 
     if(!name || !email || !password)
     {
+        const log= {
+            level: 'error',
+            message: 'Invalid registration data',
+            timestamp: new Date().toISOString(),
+            resourceId: 'invalid23',
+            traceId:uuidv4(),
+            spanId:uuidv4(),
+            commit: 'Updatedinvalid',
+           
+          };
+          fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(log),
+        });
+
         res.status(400);
         throw new Error('Please Enter all the Fields')
     }
@@ -17,6 +128,24 @@ const registerUser =asyncHandler(async(req,res)=>{
 
     if(userExists)
     {
+        const log={
+            level: 'error',
+            message: 'User already exists',
+            resourceId:'userExist'+uuidv4(),
+            timestamp: new Date().toISOString(),
+            traceId:uuidv4(),
+            spanId:uuidv4(),
+            commit: 'exists2t671',
+            
+          };
+        // app.post('http://localhost:3001/ingest',JSON.stringify(log));
+        fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(log),
+        })
         res.status(400);
         throw new Error('user already exist');
     }
@@ -31,7 +160,26 @@ const registerUser =asyncHandler(async(req,res)=>{
     // change
      await user.save()
     if(user)
-{
+{     
+    
+    const log= {
+        level: 'info',
+        message: 'User registered successfully',
+        timestamp: new Date().toISOString(),
+        resourceId: 'usercontrol'+uuidv4(),
+        traceId: uuidv4(), // Replace with actual trace ID logic
+        spanId: uuidv4(), // Replace with actual span ID logic
+        commit: 'register6728',
+
+      };
+      fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(log),
+    });
+  
     res.status(201).json({
         _id:user._id,
         name:user.name,
@@ -43,6 +191,23 @@ const registerUser =asyncHandler(async(req,res)=>{
 
 }
 else{
+    const log={
+        level: 'error',
+        message: 'Failed to create new user',
+        timestamp: new Date().toISOString(),
+        resourceId: undefined, // Replace with actual resource ID logic
+        traceId: uuidv4(), // Replace with actual trace ID logic
+        spanId: uuidv4(), // Replace with actual span ID logic
+        commit: 'Failed672',
+        
+      };
+      fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(log),
+    });
     res.status(400);
     throw new Error("Failed to create new user")
 }
@@ -56,6 +221,23 @@ const authUser =asyncHandler(async(req,res)=>{
 
     if(user && (await user.matchPassword(password)))
     {
+        const log= {
+            level: 'info',
+            message: 'User authenticated successfully',
+            timestamp: new Date().toISOString(),
+            resourceId:'match'+uuidv4(),
+            traceId: uuidv4(), // Replace with actual trace ID logic
+            spanId: uuidv4(), // Replace with actual span ID logic
+            commit: 'authenticated35465',
+           
+          };
+          fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(log),
+        });
         res.json({
              _id:user._id,
         name:user.name,
@@ -66,6 +248,27 @@ const authUser =asyncHandler(async(req,res)=>{
         })
     }
     else{
+       const log= {
+            level: 'error',
+            message: 'Invalid id or password',
+            resourceId:undefined,
+            timestamp: new Date().toISOString(),
+            traceId:uuidv4(),
+            spanId:uuidv4(),
+            commit: 'invalid oiuy4567',
+          
+          };
+          fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(log),
+        })
+    
+
+
+        //  app.post('http://localhost:3002/ingest',log);
     res.status(400);
     throw new Error("Invalid id or password")
 }
@@ -82,6 +285,25 @@ const allUsers= asyncHandler(async (req,res)=>{
     :{}
     // change from find
     const users =await User.find(keyword).find({_id: {$ne: req.user._id}});
+     const log= {
+        level: 'info',
+        message: 'Fetched all users',
+        resourceId:'user'+uuidv4(),
+        timestamp: new Date().toISOString(),
+        traceId:uuidv4(),
+        spanId:uuidv4(),
+        commit: 'Fetched23e8',
+     
+      };
+    //   app.post('http://localhost:3001/ingest',JSON.stringify(log));
+
+    fetch('https://log-ingestor-mko8.onrender.com/ingest', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(log),
+    })
     res.send(users)
 
    
